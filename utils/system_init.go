f90397485be7a0679dc2363f99505966bf255106
annidy <annidy@gmail.com>
2023-07-01 11:42:58 +0800

    打印SQL日志

--- utils/system_init.go
+++ utils/system_init.go
@@ -2,10 +2,14 @@ package utils
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var Db *gorm.DB
@@ -20,7 +24,16 @@ func InitConfig() {
 }
 
 func InitMySQL() {
-	db, err := gorm.Open(mysql.Open("root:1234@tcp(mysql8019:3306)/ginchat?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level
+			Colorful:                  true,
+			IgnoreRecordNotFoundError: true, // Ignore ErrRecordNotFound error for logger
+		},
+	)
+	db, err := gorm.Open(mysql.Open("root:1234@tcp(mysql8019:3306)/ginchat?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{Logger: newLogger})
 	if err != nil {
 		panic(err)
 	}
